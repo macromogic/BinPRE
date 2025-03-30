7a29b2d99d6bf026a672e77d63dbe51128ee8724
Weijie Huang <wh25@iu.edu>
2025-03-30 13:13:09 -0400

    update build options

---
+++ Artifact_Evaluation/BinPRE_scripts/update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -u
+DEV_CXXFLAGS=
+DO_PULL=0
+case ${1:-} in
+    --verbose-debug)
+        DEV_CXXFLAGS="${DEV_CXXFLAGS} -DBINPRE_DEBUG"
+        ;;
+    --testhook)
+        DEV_CXXFLAGS="${DEV_CXXFLAGS} -DTEST_HOOK"
+        ;;
+    --pull)
+        DO_PULL=1
+        ;;
+esac
+
+BINPRE_PATH=$(dirname $(dirname $(dirname $(realpath $0))))
+cd $BINPRE_PATH/src
+if [ $DO_PULL -eq 1 ]
+then
+    git pull
+fi
+
+export DEV_CXXFLAGS
+./run compile taint
