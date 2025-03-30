8b3cb4843dde04e09fd39d8c4d4e7104bcf4d263
Weijie Huang <wh25@iu.edu>
2025-03-30 19:01:04 -0400

    tftp dummy gt

---
+++ Analyzer/Groundtruth/tftp.py
@@ -0,0 +1,13 @@
+__all__ = [
+    'tftp_Syntax_Groundtruth',
+    'tftp_Semantic_Groundtruth',
+    'tftp_commandOffset',
+    'tftp_lengthOffset',
+    'tftp_Semantic_Functions_Groundtruth',
+]
+
+tftp_Syntax_Groundtruth = {}
+tftp_Semantic_Groundtruth = {}
+tftp_Semantic_Functions_Groundtruth = {}
+tftp_commandOffset = '-1'  # Not exist
+tftp_lengthOffset = '-1'  # Not exist
