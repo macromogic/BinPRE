385618e4c779ed968ae7b5bf913b5c7008104e66
Weijie Huang <wh25@iu.edu>
2025-04-11 16:35:41 -0400

    update mirai data

--- Analyzer/Groundtruth/mirai.py
+++ Analyzer/Groundtruth/mirai.py
@@ -8,8 +8,6 @@ __all__ = [
     'mirai_Semantic_Functions_Groundtruth',
 ]
 
-_mirai_empty_ids = [2, 7, 13, 17, 23, 27, 32, 37, 44, 51]
-
 _mirai_fields = [
     [
         Field.length(2),  # Length
@@ -19,11 +17,8 @@ _mirai_fields = [
         ...,  # Targets
         Field.length(1),  # Opt count
         ...,  # Options
-    ] for _ in range(54)
+    ] for _ in range(50)
 ]
-
-for i in _mirai_empty_ids:
-    _mirai_fields[i] = [Field.length(2)]
 
 _mirai_target_fields = [
     Field.integer(4),  # Target IP
@@ -37,18 +32,18 @@ _mirai_option_fields = [
 ]
 
 _double_target_ids = [
-    1, 3, 6, 9, 10, 12, 14, 16, 19, 21, 24, 25, 29, 31, 33, 36, 38, 40, 42, 43, 46, 48, 50, 52
+    1, 2, 3, 6, 7, 9, 10, 12, 14, 16, 19, 21, 23, 24, 25, 27, 29, 31, 33, 36, 38, 40, 42, 43, 44, 46, 48
 ]
 
 _vlens = [
     [1],
     [1, 3],
-    [],
+    [3],
     [2, 2],
     [3, 2],
     [],
     [1, 3],
-    [],
+    [2],
     [3, 3],
     [1, 2],
     [2, 3],
@@ -58,22 +53,22 @@ _vlens = [
     [1],
     [3],
     [3],
-    [],
+    [1],
     [],
     [1],
     [3, 1],
     [1],
     [3],
-    [],
+    [1, 3],
     [3],
     [2],
     [1, 3],
-    [],
+    [2, 2],
     [3, 1],
     [1],
     [3],
     [1, 3],
-    [],
+    [3, 2],
     [2],
     [3, 1],
     [],
@@ -85,22 +80,16 @@ _vlens = [
     [3],
     [3],
     [4],
-    [],
+    [1, 3],
     [4, 2],
     [],
     [4],
     [1],
     [3, 1],
-    [3],
-    [],
-    [2],
-    [],
 ]
 
 _vfields = {}
 for i, v in enumerate(_vlens):
-    if i in _mirai_empty_ids:
-        continue
     if i in _double_target_ids:
         _vfields[i] = [_mirai_target_fields[::] * 2]
     else:
